75c11f8b1e44f9a93db2b783c9f4f6ac789d9712
TakSeBiegam <kurylo.ao@gmail.com>
2024-05-11 18:27:19 +0200

    wip

--- src/test.ts
+++ src/test.ts
@@ -1,10 +1,21 @@
-import { Options, Parser, ParserField, TreeToGraphQL } from 'graphql-js-tree';
+import { FieldType, Options, Parser, ParserField, TypeDefinition } from 'graphql-js-tree';
 
 const input = `
 type Post{
   name: String!
   content: String!
   createdAt: String!
+  comments: Comment!
+}
+
+type Comment {
+  title: String!
+  isLiked: Likes!
+}
+
+type Likes {
+  Liked: Boolean!
+  Loved: Boolean!
 }
 
 type Query{
@@ -17,28 +28,22 @@ type PublicQuery{
 }
 
 type User {
-  username: String!
-  sex: Sex!
+  username: String! @constraint(format: "^[0-9a-zA-Z]*$")
+  nickname: String! @constraint(maxLength: 2137)
+  sex: SEX
+  friends: [String!]!
 }
 
-type UserQuery {
+tysdasdsape UserQuery {
   me: User!
-  getPosts(
-    input: getPostsInput
-  ): [Post!]
   getPost(
     id: String!
   ): Post!
 }
 
-input getPostsInput {
-  content: String!
-  fromTime: Int
-}
-
-enum Sex {
-  F
+enum SEX {
   M
+  F
 }
 
 schema {
@@ -47,26 +52,112 @@ schema {
 `;
 
 const GqlScalars = ['INT', 'FLOAT', 'STRING', 'BOOLEAN', 'ID'];
+type GqlEnum = {
+  name: string;
+  fields: string[];
+};
+
+const handleConstraintDirective = (arg: ParserField) => {
+  console.log(arg);
+  switch (arg.name) {
+    case 'maxLength':
+      return `(${arg.value?.value})`;
+    case 'format':
+    case 'pattern':
+      return ` ${arg.value?.value}`;
+    default:
+      console.error(`NOT HANDLED CONSTRAINT DIRECTIVE ARGUMENT ${arg.name}`);
+      break;
+  }
+};
+
+const convertDirective = (directive: ParserField) => {
+  console.log(directive);
+  switch (directive.name) {
+    case 'constraint': {
+      return handleConstraintDirective(directive.args[0]);
+    }
+    default:
+      console.error('NOT HANDLED DIRECTIVE');
+  }
+};
 
 const convertScalarsToUpperCase = (input: string): string =>
   GqlScalars.indexOf(input.toUpperCase()) !== -1 ? input.toUpperCase() : input;
 
+const checkFieldTypeIsScalar = (enumName: string, enumArray: GqlEnum[]) =>
+  enumArray.some((enumObj) => enumObj.name === enumName);
+
+const findEnumByName = (enumName: string, enumArray: GqlEnum[]): GqlEnum => {
+  const e = enumArray.find((enumObj) => enumObj.name === enumName);
+  if (!e) {
+    throw new Error('UNKNOWN ENUM TYPE');
+  }
+  return e;
+};
+
+const convertToEnumOrScalar = (
+  obj: {
+    type: Options.name;
+    name: string;
+  },
+  enumArray: GqlEnum[],
+) =>
+  checkFieldTypeIsScalar(obj.name, enumArray)
+    ? `ENUM(${findEnumByName(obj.name, enumArray).fields.map((f) => `"${f}"`)}) `
+    : convertScalarsToUpperCase(obj.name);
+
+const convertToArrayScalar = (obj: { type: Options.array; nest: FieldType }) => {
+  if (obj.nest.type === Options.required) {
+    if (obj.nest.nest.type === Options.name) {
+      return `${convertScalarsToUpperCase(obj.nest.nest.name)} ARRAY`;
+    }
+  }
+  console.error(`NOT HANDLED TYPE (got: ${obj})`);
+  return '<UNKNOWN>';
+};
+
+const getEnums = (nodes: ParserField[]) => {
+  const enums = nodes.map((node): GqlEnum | undefined => {
+    if (node.type.fieldType.type === Options.name && node.type.fieldType.name === 'enum') {
+      return {
+        fields: node.args.map((a) => a.name),
+        name: node.name,
+      };
+    }
+  });
+  return enums.filter((e): e is GqlEnum => !!e);
+};
+
+const checkIfNodeIsObject = (obj: ParserField, nodes: ParserField[]) =>
+  nodes.some((node) => obj.type.fieldType.type === Options.name && node.id === obj.type.fieldType.name);
+
+const getObjects = (nodes: ParserField[]) =>
+  nodes.filter((node) => node.data.type === TypeDefinition.ObjectTypeDefinition);
+
 const CreateGraphWithoutInputs = (nodes: ParserField[]) => {
   let result = '';
+  let enumArray = getEnums(nodes);
+  let objectsArray = getObjects(nodes);
   for (let index = 0; index < nodes.length; index++) {
     const node = nodes[index];
+    if (node.type.fieldType.type === Options.name && node.type.fieldType.name === 'enum') continue;
     result += node.args.every((arg) => arg && arg.args && arg.args.length > 0)
       ? ''
       : `\n  (${node.name}: ${node.name} {` +
         node.args.map((arg) => {
           const isRequired = arg.type.fieldType.type === Options.required;
+          const isNested = checkIfNodeIsObject(arg, objectsArray);
           const curArg =
             arg.type.fieldType.type === Options.name
-              ? convertScalarsToUpperCase(arg.type.fieldType.name)
+              ? convertToEnumOrScalar(arg.type.fieldType, enumArray)
               : arg.type.fieldType.nest.type === Options.name
               ? convertScalarsToUpperCase(arg.type.fieldType.nest.name)
-              : '<UNKNOWN>';
-          return ` ${arg.name}: ${curArg}${isRequired ? '!' : ''}`;
+              : arg.type.fieldType.nest.type === Options.array
+              ? convertToArrayScalar(arg.type.fieldType.nest)
+              : (console.error(`NOT HANDLED TYPE (got: ${node})`), '<UNKNOWN>');
+          const dir = arg.directives.length ? convertDirective(arg.directives[0]) : undefined;
+          return `${isRequired ? '' : ' OPTIONAL'} ${isNested ? curArg : arg.name + ' ' + curArg}${!!dir ? dir : ''}`;
         }) +
         ` }),`;
   }
@@ -111,12 +202,12 @@ const convertGraph = (title: string, nodes: ParserField[]) => {
     `\n}`
   );
 };
-
-// const rawGql = tokenizeGraphql(input);
-const nodes = Parser.parse(input).nodes.filter((node) => node.name !== 'schema');
+let nodes: ParserField[] = [];
+try {
+  nodes = Parser.parse(input).nodes.filter((node) => node.name !== 'schema');
+} catch (e) {
+  console.error(`your schema is not valid.
+${e}`);
+  throw new Error('not valid schema');
+}
 console.log(convertGraph('schema', nodes));
-// console.log(JSON.parse(JSON.stringify(nodes[0].args[0].type.fieldType)));
-// const preParsedGql = findParentForNodes(rawGql);
-// console.log(preParsedGql);
-// const pgGraph = createGraph("First", preParsedGql);
-// console.log(pgGraph);
